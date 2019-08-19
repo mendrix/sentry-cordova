4f04fd410ee19e7394b6f247abdcbe950d615094
Owen <hardy.owen@gmail.com>
2019-08-19 12:27:50 +0200

    added showReportDialog

--- src/js/client.ts
+++ src/js/client.ts
@@ -1,6 +1,7 @@
-import { BaseClient, Scope } from '@sentry/core';
+import {ReportDialogOptions} from "@sentry/browser";
+import { API, BaseClient, Scope } from '@sentry/core';
 import { Event, EventHint } from '@sentry/types';
-import { SyncPromise } from '@sentry/utils';
+import { getGlobalObject, logger, SyncPromise } from '@sentry/utils';
 
 import { CordovaBackend, CordovaOptions } from './backend';
 import { SDK_NAME, SDK_VERSION } from './version';
@@ -39,5 +40,46 @@ export class CordovaClient extends BaseClient<CordovaBackend, CordovaOptions> {
     };
 
     return super._prepareEvent(event, scope, hint);
+  }
+
+
+  /**
+   * Show a report dialog to the user to send feedback to a specific event.
+   *
+   * @param options Set individual options for the dialog
+   */
+  public showReportDialog(options: ReportDialogOptions = {}): void {
+    // doesn't work without a document (React Native)
+    const document = getGlobalObject<Window>().document;
+    if (!document) {
+      return;
+    }
+
+    if (!this._isEnabled()) {
+      logger.error('Trying to call showReportDialog with Sentry Client is disabled');
+      return;
+    }
+
+    const dsn = options.dsn || this.getDsn();
+
+    if (!options.eventId) {
+      logger.error('Missing `eventId` option in showReportDialog call');
+      return;
+    }
+
+    if (!dsn) {
+      logger.error('Missing `Dsn` option in showReportDialog call');
+      return;
+    }
+
+    const script = document.createElement('script');
+    script.async = true;
+    script.src = new API(dsn).getReportDialogEndpoint(options);
+
+    if (options.onLoad) {
+      script.onload = options.onLoad;
+    }
+
+    (document.head || document.body).appendChild(script);
   }
 }
